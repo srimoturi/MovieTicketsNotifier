fb99bce8b9cf0e17553ffd1117498e576a553bc2
Sritej Moturi <sritej.moturi@mutualmobile.com>
2016-11-30 15:40:07 +0530

    Added htmlparse in depndencies. added manifest

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 
-var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
+//
+// // views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
